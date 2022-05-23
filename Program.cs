5ae9280f65b255fd197075af50e766c08f71b835
Anna Beatriz <annab_oliver@hotmail.com>
2022-05-22 23:10:42 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿const string senhaCorreta = "1234abcd";
+string senha, permissaoAcesso;
+
+Console.Write("Olá, usuário. Por favor, digite sua senha:");
+senha = Console.ReadLine()!;
+
+permissaoAcesso = senha == senhaCorreta ? "permitido" : "negado";
+
+Console.WriteLine($" Acesso {permissaoAcesso}");
